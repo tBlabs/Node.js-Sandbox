6ec8b4bfab91a6e54f0f0e7b24ddacbe60b51d13
tB <tomasz.budrewicz@specteam.pl>
2017-04-30 15:07:30 +0200

    TS builder + debugger | initial

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+class Startup
+{
+    public static Main(): number
+    {
+        console.log("Startup()");
+
+        let a = 5;
+        let b = 7;
+
+        let c = a + b;
+
+        return 0;
+    }
+}
+
+Startup.Main();
